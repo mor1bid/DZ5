964b6f142c85810011c9ba61e66393c222243cd0
De <spark49942@gmail.com>
2022-10-07 19:40:44 +0300

    l-f

--- smn.py
+++ smn.py
@@ -52,11 +52,62 @@ def f(x):
 lisst = [(i, f(i)) for i in range(1, 21) if i % 2 == 0]
 print(lisst)
 
-num = []
-with open('l.txt', 'r') as dig:
-    for i in dig:
-        n = dig.read()
-        num.append(n)
-print(num)
-# stripe = [(i, f(i)) for i in num]
+# num = []
+# with open('l.txt', 'r') as dig:
+#     for i in dig:
+#         num.append(i)
+# num = str(num).split()
+# print(num)
+# stripe = [(i, f(i)) for i in num if num.isdigit()]
 # print(stripe)
+
+dig = open('l.txt', 'r')
+data = dig.read() + ' '
+dig.close()
+
+num = []
+
+while data != '':
+    space_pos = data.index(' ')
+    num.append(int(data[:space_pos]))
+    data = data[space_pos+1:]
+
+out = []
+for e in num:
+    if not e % 2:
+        out.append((e,e **2))
+print(out)
+
+# map (использвать только 1 раз)
+
+li = [x for x in range(1,20)]
+
+li = list(map(lambda x:x+10, li))
+
+print(li)
+
+mydata = map(int,input().split())
+
+for e in mydata:
+    print(e)
+
+print('--')
+
+for e in mydata:
+    print(e)
+
+data = [x for x in range(10)]
+
+res = list(filter(lambda x: x%2==0, data))
+print(res)
+
+boys = ['biba69', 'boba13', 'beb12a']
+ip = [1, 2, 3]
+money = ['none']
+
+users = ['user1', 'user2', 'user3', 'user4', 'user5']
+ids = [4, 5, 9, 14, 7]
+
+data = list(zip(boys, ip))
+# data = list(enumerate(zip(users, ids)))
+print(data)
