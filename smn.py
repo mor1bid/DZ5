8122142c8736905f4bd43d1b4e8039dd2a71b574
De <spark49942@gmail.com>
2022-10-10 03:25:45 +0300

    DZ3-b

--- smn.py
+++ smn.py
@@ -8,7 +8,7 @@ line3 = '. . .'.split()
 round = 0
 move = 0
 print('\n', line1, '\n', line2, '\n', line3, '\n')
-while round < 3:
+while round < 9:
     if move == 0:
         x = int(input('Игрок 1, введите желаемую позицию: '))
         if x >-1 and x <= 2:
@@ -46,25 +46,25 @@ while round < 3:
     print('\n', line1, '\n', line2, '\n', line3, '\n')
     move -= 1
     round += 1
-if line1[0] == 'x' and line1[2]==line1[1]==line1[0] or line3[2]==line2[1]==line1[0] or line3[0]==line2[0]==line1[0]:
-    print('Игрок 1 побеждает!')
-elif line1[0] == 'o' and line1[2]==line1[1]==line1[0] or line3[2]==line2[1]==line1[0] or line3[0]==line2[0]==line1[0]:
-    print('Игрок 2 побеждает!')
-elif line1[0] == 'x' and line3[1]==line2[1]==line1[1]:
-    print('Игрок 1 побеждает!')
-elif line1[0] == 'o' and line3[1]==line2[1]==line1[1]:
-    print('Игрок 2 побеждает!')
-elif line1[0] == 'x' and line3[2]==line2[2]==line1[2] or line3[0]==line2[1]==line1[2]:
-    print('Игрок 1 побеждает!')
-elif line1[0] == 'o' and line3[2]==line2[2]==line1[2] or line3[0]==line2[1]==line1[2]:
-    print('Игрок 2 побеждает!')
-elif line1[0] == 'x' and line2[2]==line2[1]==line2[0]:
-    print('Игрок 1 побеждает!')
-elif line1[0] == 'o' and line2[2]==line2[1]==line2[0]:
-    print('Игрок 2 побеждает!')
-elif line1[0] == 'x' and line3[2]==line3[1]==line3[0]:
-    print('Игрок 1 побеждает!')
-elif line1[0] == 'o' and line3[2]==line3[1]==line3[0]:
-    print('Игрок 2 побеждает!')
-else:
-    round==0
+    if line1[0] == 'x' and line1[2]==line1[1]==line1[0] or line3[2]==line2[1]==line1[0] or line3[0]==line2[0]==line1[0]:
+        print('Игрок 1 побеждает!')
+    elif line1[0] == 'o' and line1[2]==line1[1]==line1[0] or line3[2]==line2[1]==line1[0] or line3[0]==line2[0]==line1[0]:
+        print('Игрок 2 побеждает!')
+    elif line1[0] == 'x' and line3[1]==line2[1]==line1[1]:
+        print('Игрок 1 побеждает!')
+    elif line1[0] == 'o' and line3[1]==line2[1]==line1[1]:
+        print('Игрок 2 побеждает!')
+    elif line1[0] == 'x' and line3[2]==line2[2]==line1[2] or line3[0]==line2[1]==line1[2]:
+        print('Игрок 1 побеждает!')
+    elif line1[0] == 'o' and line3[2]==line2[2]==line1[2] or line3[0]==line2[1]==line1[2]:
+        print('Игрок 2 побеждает!')
+    elif line1[0] == 'x' and line2[2]==line2[1]==line2[0]:
+        print('Игрок 1 побеждает!')
+    elif line1[0] == 'o' and line2[2]==line2[1]==line2[0]:
+        print('Игрок 2 побеждает!')
+    elif line1[0] == 'x' and line3[2]==line3[1]==line3[0]:
+        print('Игрок 1 побеждает!')
+    elif line1[0] == 'o' and line3[2]==line3[1]==line3[0]:
+        print('Игрок 2 побеждает!')
+    else:
+        round==0
